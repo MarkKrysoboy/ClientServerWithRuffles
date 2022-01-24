a5a3207032e5adcd2d948aabc6f6ecaade0aaf8a
Mark Krysoboy <heter1209@gmail.com>
2022-01-24 10:02:32 +0300

    Added try-catch for server

--- src/Server.java
+++ src/Server.java
@@ -11,52 +11,54 @@ public class Server {
         int port = 8082;
 
         while (true) {
-            ServerSocket serverSocket = new ServerSocket(port);
-            Socket clientSocket = serverSocket.accept();
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            try (ServerSocket serverSocket = new ServerSocket(port);
+                 Socket clientSocket = serverSocket.accept();
+                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
 
-            System.out.println("New connection accepted");
+                System.out.println("New connection accepted");
 
 
-            out.println("Hi client, whats your name?");
-            final String name = in.readLine();
-            out.println(String.format("Hi %s, how old are you?", name));
+                out.println("Hi client, whats your name?");
+                final String name = in.readLine();
+                out.println(String.format("Hi %s, how old are you?", name));
 
-            try {
-                final int age = Integer.parseInt(in.readLine());
-                if (age < 18) {
-                    out.println(String.format("%s, since you are under 18 years old, you will be redirected to the " +
-                            "children's segment of the network. Do you agree (yes/no)?", name));
-                } else {
-                    out.println(String.format("%s, You're old enough, I'm giving you unlimited access. Do you agree (yes/no)?",
+                try {
+                    final int age = Integer.parseInt(in.readLine());
+                    if (age < 18) {
+                        out.println(String.format("%s, since you are under 18 years old, you will be redirected to the " +
+                                "children's segment of the network. Do you agree (yes/no)?", name));
+                    } else {
+                        out.println(String.format("%s, You're old enough, I'm giving you unlimited access. Do you agree (yes/no)?",
+                                name));
+                    }
+                } catch (NumberFormatException e) {
+                    out.println(String.format("%s, since you don't distinguish a number from a string, you will be " +
+                                    "redirected to the children's segment of the network. Do you agree (yes/no)?",
                             name));
                 }
-            } catch (NumberFormatException e) {
-                out.println(String.format("%s, since you don't distinguish a number from a string, you will be " +
-                                "redirected to the children's segment of the network. Do you agree (yes/no)?",
+
+                if (in.readLine().equals("yes")) {
+                    out.println(String.format("OK, %s, you're connected. What do you want to find?",
+                            name));
+                } else {
+                    out.println("Well, if you don't agree, I will filter the result anyway. What do you want to find?");
+                }
+
+                out.println(String.format("%s, I 'm already looking \"" + in.readLine() + "\"... Enter \"end\" if you get tired of waiting.",
                         name));
+
+                if (in.readLine().equals("end")) {
+                    out.println(String.format("Goodbye, %s, glad to talk to you!",
+                            name));
+                } else {
+                    out.println(String.format("I don't now, what do you want. Goodbye, %s, glad to talk to you!",
+                            name));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
 
-            if (in.readLine().equals("yes")) {
-                out.println(String.format("OK, %s, you're connected. What do you want to find?",
-                        name));
-            } else {
-                out.println("Well, if you don't agree, I will filter the result anyway. What do you want to find?");
-            }
-
-            out.println(String.format("%s, I 'm already looking \"" + in.readLine() + "\"... Enter \"end\" if you get tired of waiting.",
-                    name));
-
-            if (in.readLine().equals("end")) {
-                out.println(String.format("Goodbye, %s, glad to talk to you!",
-                        name));
-            } else {
-                out.println(String.format("I don't now, what do you want. Goodbye, %s, glad to talk to you!",
-                        name));
-            }
-            clientSocket.close();
-            serverSocket.close();
         }
     }
 
